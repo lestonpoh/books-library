6a3c6cf31599c059867d5d1dfe061670128bdc71
Leston <129874349+lestonpoh@users.noreply.github.com>
2023-09-02 18:09:24 +0800

    add remove button

--- script.js
+++ script.js
@@ -27,36 +27,83 @@ function addBookToLibrary(newBook) {
 
 const booksContent = document.querySelector(".books-content")
 
-function createCard(book){
+function createCard(book,index){
     const card = document.createElement("div");
     const title = document.createElement("p");
     const author = document.createElement("p");
     const pages = document.createElement("p");
     const read = document.createElement("p");
+    const remove = document.createElement("button")
 
     card.classList.add("card")
+    card.setAttribute("id",index)
 
     booksContent.appendChild(card);
     card.appendChild(title);
     card.appendChild(author);
     card.appendChild(pages);
     card.appendChild(read);
+    card.appendChild(remove)
 
     title.textContent = book.title;
     author.textContent = book.author;
     pages.textContent = book.pages;
     read.textContent = book.read === true ? "Read": "Not Read";
+    remove.classList.add("remove");
+    remove.textContent = "Remove"
+}
+
+function removeBook(index){
+    myLibrary.splice(index,1)
 
 }
 
+function displayBooks(){
+    booksContent.innerHTML="";
+    myLibrary.forEach((book,index) => {
+        createCard(book,index)
+    });
+    
+    const removeButtons = document.querySelectorAll(".remove");
+    removeButtons.forEach((removeButton)=>{
+        removeButton.addEventListener("click",(e)=>{
+            console.log(e.target.parentElement.id)
+            removeBook(e.target.parentElement.id)
+            console.log(myLibrary)
+            displayBooks()
+        })
+    })
+}
 
-myLibrary.forEach((book) => {
-    createCard(book)
-});
+function resetPopup(title,author,pages,read){
+    title="";
+    console.log(title.value)
+    author="";
+    pages="";
+    read = false
+}
+
+
+
+// first display
+displayBooks()
+
+// button to open popup
+const addBookButton = document.querySelector("#add-book");
+const popup = document.querySelector(".pop-up");
+addBookButton.addEventListener("click",(e)=>{
+    popup.classList.add("open-pop-up")
+})
+
+// button to close popup
+const closePopupButton = document.querySelector("#close");
+closePopupButton.addEventListener("click",(e)=>{
+    popup.classList.remove("open-pop-up");
+    form.reset();
+})
 
 const form = document.querySelector("form");
-const submit = document.querySelector("#submit")
-submit.addEventListener("click",(e)=>{
+form.addEventListener("submit",(e)=>{
     e.preventDefault();
     let title = document.querySelector("#title").value;
     let author = document.querySelector("#author").value;
@@ -72,21 +119,15 @@ submit.addEventListener("click",(e)=>{
         }
     })
     if (bookExist) {return};
+
+
     newBook = new Book(title,author,pages,read);
     addBookToLibrary(newBook);
-    booksContent.innerHTML="";
-    myLibrary.forEach((book) => {
-        createCard(book);
-    });
+    displayBooks();
+    form.reset();
+    
+    console.log("ewdd"+title)
+    popup.classList.remove("open-pop-up");
 })
 
-const addBookButton = document.querySelector("#add-book");
-const popup = document.querySelector(".pop-up");
-addBookButton.addEventListener("click",(e)=>{
-    popup.classList.add("open-pop-up")
-})
 
-const closePopupButton = document.querySelector("#close");
-closePopupButton.addEventListener("click",(e)=>{
-    popup.classList.remove("open-pop-up")
-})
